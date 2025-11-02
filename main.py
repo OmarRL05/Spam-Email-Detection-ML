0deb12ebc130855c59c458b1fb9e5a5f7819737b
Omar Reyes L. <relo2210@gmail.com>
2025-11-02 16:14:24 -0600

    feat: Extract plain-text body from raw emails
    Uses the email.walk() method to parse raw, multipart emails. This extracts all 'text/plain' and 'text/html' parts, ignoring attachments (like images).
    
    -Provides a much cleaner text body for the next steps (HTML stripping and NLP), which will improve the quality of the data used to train the model.

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
 import os
 from html_parser import MyHTMLParser
+import email
+
+def get_email_body(email_message):
+    msg = email.message_from_string(email_message)
+    body = ""
+    for part in msg.walk():
+        content_type = part.get_content_type()
+        if content_type == 'text/plain' or content_type == 'text/html':
+            try:
+                payload = part.get_payload(decode=True)
+                body += payload.decode('latin-1')
+            except:
+                pass
+    return  body
 
 def strip_tags(c_mail):
+    """Cleans a raw email string by extracting and stripping its content.
+
+        This function acts as a pipeline that first uses `get_email_body`
+        to parse the raw email (handling multipart messages) and then
+        uses `MyHTMLParser` to strip any remaining HTML tags.
+
+        :param c_mail: The raw email content as a string.
+        :return: A single string of cleaned, plain-text.
+    """
+    email_body = get_email_body(c_mail)
     html_stripper = MyHTMLParser()
-    html_stripper.feed(c_mail)
+    html_stripper.feed(email_body)
     return html_stripper.return_data()
 
 def read_data(file='../email-dataset/full/index', amount=50):
     """
     Reads the Dataset Files
 
-    :param file: The dataset file direction
+    :param file: The dataset's index file.
     :param amount: The amount of data to be read
     :return: List with spam/ham labels and emails content
     """
